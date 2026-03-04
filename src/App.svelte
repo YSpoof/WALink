0d9835ea8d7b1abf74d78431eb5fd41ed018bc89
YSpoof <32226366+YSpoof@users.noreply.github.com>
2026-03-04 08:16:21 -0300

    Feat: Add test button

--- src/App.svelte
+++ src/App.svelte
@@ -153,6 +153,12 @@
             class="uppercase w-full bg-green-500 text-white px-4 py-2 rounded-md"
             >{#if copied}Copiado!{:else}Copiar{/if}</button
           >
+          <a
+            href={fullLink}
+            target="_blank"
+            class="uppercase w-full bg-blue-500 text-white px-4 py-2 rounded-md"
+            >Testar</a
+          >
           <button
             onclick={() => successDialog?.close()}
             class="uppercase w-full px-4 py-2 rounded-md border">Fechar</button
